139010df3deeafa8f128bc76b56c2a6eef5db3ea
supernomad Christian Saide <csaide@quantumnet.ninja>
2016-02-23 00:24:24 -0500

    Rebuilt basic structure.

---
+++ app/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const electron = require('electron'),
+	app = electron.app,  // Module to control application life.
+	BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+var mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform != 'darwin') {
+		app.quit();
+	}
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function() {
+	// Create the browser window.
+	mainWindow = new BrowserWindow({width: 800, height: 600});
+
+	// and load the index.html of the app.
+	mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+	mainWindow.on('closed', function() {
+		mainWindow = null;
+	});
+});
